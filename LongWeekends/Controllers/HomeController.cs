ace8efcbdaf60a8f05ee805c1f42b69389d3e64c
Hasan Kaplan <hasankaplan.kariyer@gmail.com>
2026-05-03 12:41:42 +0300

    update: Endpoint için 90 gün kontrolü eklendi. Reference Updates

--- LongWeekends/Controllers/HomeController.cs
+++ LongWeekends/Controllers/HomeController.cs
@@ -34,6 +34,12 @@ public class HomeController : Controller
     [HttpPost]
     public async Task<JsonResult> GetDayGridMonthEvents(DateTime startDateTime, DateTime endDateTime, CancellationToken cancellationToken = default)
     {
+        var diff = endDateTime - startDateTime;
+        if(diff > TimeSpan.FromDays(90))
+        {
+            throw new ArgumentException("Date range should not exceed 90 days.");
+        }
+
         var longWeekendDays = _specialDayService.GetLongWeekendDays(startDateTime, endDateTime, [Country.Turkiye]);
         return Json(longWeekendDays);
     }
